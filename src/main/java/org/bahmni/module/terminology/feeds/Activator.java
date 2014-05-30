945559796f3cb75a957caa34bbf4ccdb065cb29f
Johnvp <johnvp@john.corporate.thoughtworks.com>
2014-05-30 17:50:52 +0530

    John, Balaji  | initial commit

---
+++ src/main/java/org/bahmni/module/terminology/feeds/Activator.java
@@ -0,0 +1,21 @@
+package org.bahmni.module.terminology.feeds;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.module.BaseModuleActivator;
+
+public class Activator extends BaseModuleActivator {
+
+    private Log log = LogFactory.getLog(this.getClass());
+
+    @Override
+    public void started() {
+        log.info("Started the Terminology Atom Feed Client module");
+    }
+
+    @Override
+    public void stopped() {
+        log.info("Stopped the Terminology Atom Feed Client module");
+    }
+}
---
+++ src/main/java/org/bahmni/module/terminology/feeds/ConceptFeedClient.java
@@ -0,0 +1,8 @@
+package org.bahmni.module.terminology.feeds;
+
+import org.springframework.stereotype.Component;
+
+@Component("conceptFeedClient")
+public class ConceptFeedClient {
+
+}
